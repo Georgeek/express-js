4af97fb3373d36f24c3bd4744f2a630313b28117
Georgeek <egormad@gmail.com>
2017-03-09 11:48:39 +0800

    bubble mentoring section + js

--- 40-express-generator/bootstrap-express/public/javascripts/function.js
+++ 40-express-generator/bootstrap-express/public/javascripts/function.js
@@ -7,3 +7,24 @@ function youtubeVidScroll() {
 	$('.video-strip').css('background-position', 'center -'+ wScroll +'px');
 	// console.log(wScroll);
 }
+
+$(function() {
+	mentoringBubbleClick();
+});
+
+function mentoringBubbleClick() {
+	$('.face').on('click', function() {
+		var faceTop = $(this).position().top,
+			verticalMath = faceTop - 230;
+		
+		console.log(verticalMath);
+		$(this).addClass('has-bubble-open')
+			.siblings().removeClass('has-bubble-open');
+
+		// when i click a face get the distance of the face from its parent
+		// and then move the whole container up 115px + count oh hover
+		// after we add is-open class to the face, pop the baloon
+		$
+
+	});
+}
