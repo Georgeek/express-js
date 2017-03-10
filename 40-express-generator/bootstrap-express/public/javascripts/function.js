98aebb24ac0f195b3589bad752a9c0d30d1c406b
Georgeek <egormad@gmail.com>
2017-03-10 20:53:22 +0800

    refactoring js

--- 40-express-generator/bootstrap-express/public/javascripts/function.js
+++ 40-express-generator/bootstrap-express/public/javascripts/function.js
@@ -1,64 +1,30 @@
 (function ($) {
 	"use strict"
-	
-	var 
-		$window = $(window),
+
+	var	$window = $(window),
+		$this = $(this),
 		$face = $('.face'),
 		$faces = $('.faces');
 
-	$window.scroll(function() {
-		youtubeVidScroll();
-		startMentoring();
-	});
-
-	$(function() {
+	$(document).ready(function() {
 		mentoringBubbleClick();
 	});
 
-	$window.resize(function() {
-		if( $(window).width() > 640 ) {
-			mentoringWideStart();
-		} else {
-			mentoringNarrowStart();
-		}
+	$window.scroll(function() {
+		scrollingSections();
 	});
 
-	function youtubeVidScroll() {
-		var wScroll = $(window).scrollTop();
+// scroll down faces pop up
+	function scrollingSections() {
+		var wScroll = $window.scrollTop();
 		$('.video-strip').css('background-position', 'center -'+ wScroll +'px');
-	}
 
-	function mentoringBubbleClick() {
-		$face.on('click', function() {
-			var faceTop = $(this).position().top,
-				verticalMath = -1 * (faceTop - 230),
-				faceLeft = $(this).position().left,
-				horizontalMath = 0 - faceLeft;
-
-			if( $window.width() > 640 ) {
-				$(this).parent().css('top', + verticalMath +'px');
-			} else {
-				if( $(this).hasClass('back-btn') ) {
-					mentoringNarrowStart();
-				} else {
-					$(this).parent().css('left', + horizontalMath +'px');
-				}
-			}
-			if( !$(this).hasClass('back-btn')) {
-				$(this).addClass('has-bubble-open')
-				.siblings().removeClass('has-bubble-open');
-			}
-		});
-	}
-
-	function startMentoring() {
-		var wScroll = $(window).scrollTop();
 		// console.log(wScroll);
-		if($('section.mentoring').offset().top - 300 < wScroll) {
+		if($('section.mentoring').offset().top - 500 < wScroll) {
 			if( $window.width() > 640 ) {
-				$('.faces').addClass('launched');
-				
-				if( !$('.face').hasClass('has-bubble-open') && $(this).hasClass('back-btn')) {
+				$faces.addClass('launched');
+
+				if( !$face.hasClass('has-bubble-open')) {
 					setTimeout(function() {
 						$('.face:nth-child(3)').addClass('has-bubble-open');
 					}, 500);
@@ -69,20 +35,63 @@
 		}
 	}
 
+// mentoring section, resizing, activating differ tooltips
+	$window.resize(function() {
+		if( $window.width() > 640 ) {
+			mentoringWideStart();
+		} else {
+			mentoringNarrowStart();
+		}
+	});
+
 	function mentoringWideStart() {
-		$('.faces').css({
+		$faces.css({
 			'top': '0',
 			'left': '0'
 		});
-		$('.face:nth-child(3)').addClass('has-bubble-open').siblings().removeClass('has-bubble-open');
+
+		$('.face:nth-child(3)').addClass('has-bubble-open')
+		.siblings().removeClass('has-bubble-open');
 	}
 
 	function mentoringNarrowStart() {
-		$('.faces').css({
+		$faces.css({
 			'top': '230px',
 			'left': '0'
 		});
-		$('.face').first().addClass('has-bubble-open').siblings().removeClass('has-bubble-open');
+
+		$face.first().addClass('has-bubble-open')
+		.siblings().removeClass('has-bubble-open');
+	}
+
+// click faces on mentoring section
+	function mentoringBubbleClick() {
+		$face.on('click', onFaceClick);
+	}
+
+	function onFaceClick() {
+		var $this = $(this),
+			$siblings = $this.siblings(),
+			$parent = $this.parent(),
+
+			faceTop = $this.position().top,
+			verticalMath = -1 * (faceTop - 230),
+			faceLeft = $this.position().left,
+			horizontalMath = 0 - faceLeft;
+
+		if( $window.width() > 640 ) {
+			$parent.css('top', verticalMath +'px');
+		} else {
+			if( $this.hasClass('back-btn') ) {
+				mentoringNarrowStart();
+			} else {
+				$parent.css('left', horizontalMath +'px');
+			}
+		}
+		if( !$this.hasClass('back-btn')) {
+			$this.addClass('has-bubble-open');
+			$siblings.removeClass('has-bubble-open');
+		}
 	}
 
 })(jQuery);
