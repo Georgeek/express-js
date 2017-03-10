703ba61cc1ff2f8d3e316786a0f53c04f3ce0acb
Georgeek <egormad@gmail.com>
2017-03-10 16:04:45 +0800

    refactoring js

--- 40-express-generator/bootstrap-express/public/javascripts/function.js
+++ 40-express-generator/bootstrap-express/public/javascripts/function.js
@@ -1,78 +1,88 @@
-$(window).scroll(function() {
-	youtubeVidScroll();
-	startMentoring();
-});
+(function ($) {
+	"use strict"
+	
+	var 
+		$window = $(window),
+		$face = $('.face'),
+		$faces = $('.faces');
 
-$(function() {
-	mentoringBubbleClick();
-});
-
-$(window).resize(function() {
-	if( $(window).width() > 640 ) {
-		mentoringWideStart();
-	} else {
-		mentoringNarrowStart();
-	}
-});
-
-function youtubeVidScroll() {
-	var wScroll = $(window).scrollTop();
-	$('.video-strip').css('background-position', 'center -'+ wScroll +'px');
-}
-
-function mentoringBubbleClick() {
-	$('.face').on('click', function() {
-		var faceTop = $(this).position().top,
-			verticalMath = -1 * (faceTop - 230),
-			faceLeft = $(this).position().left,
-			horizontalMath = 0 - faceLeft;
-
-		if( $(window).width() > 640 ) {
-			$(this).parent().css('top', + verticalMath +'px');
-		} else {
-			if( $(this).hasClass('back-btn') ) {
-				mentoringNarrowStart();
-			} else {
-				$(this).parent().css('left', + horizontalMath +'px');
-			}
-		}
-		if( !$(this).hasClass('back-btn')) {
-			$(this).addClass('has-bubble-open')
-			.siblings().removeClass('has-bubble-open');
-		}
+	$window.scroll(function() {
+		youtubeVidScroll();
+		startMentoring();
 	});
-}
 
-function startMentoring() {
-	var wScroll = $(window).scrollTop();
+	$(function() {
+		mentoringBubbleClick();
+	});
 
-	if($('section.mentoring').offset().top - 500 < wScroll) {
+	$window.resize(function() {
 		if( $(window).width() > 640 ) {
-			$('.faces').addClass('launched');
-			
-			if( !$('.face').hasClass('has-bubble-open') && $(this).hasClass('back-btn')) {
-				setTimeout(function() {
-					$('.face:nth-child(3)').addClass('has-bubble-open');
-				}, 500);
-			}
+			mentoringWideStart();
 		} else {
 			mentoringNarrowStart();
 		}
+	});
+
+	function youtubeVidScroll() {
+		var wScroll = $(window).scrollTop();
+		$('.video-strip').css('background-position', 'center -'+ wScroll +'px');
 	}
-}
 
-function mentoringWideStart() {
-	$('.faces').css({
-		'top': '0',
-		'left': '0'
-	});
-	$('.face:nth-child(3)').addClass('has-bubble-open').siblings().removeClass('has-bubble-open');
-}
+	function mentoringBubbleClick() {
+		$face.on('click', function() {
+			var faceTop = $(this).position().top,
+				verticalMath = -1 * (faceTop - 230),
+				faceLeft = $(this).position().left,
+				horizontalMath = 0 - faceLeft;
 
-function mentoringNarrowStart() {
-	$('.faces').css({
-		'top': '230px',
-		'left': '0'
-	});
-	$('.face').first().addClass('has-bubble-open').siblings().removeClass('has-bubble-open');
-}
+			if( $window.width() > 640 ) {
+				$(this).parent().css('top', + verticalMath +'px');
+			} else {
+				if( $(this).hasClass('back-btn') ) {
+					mentoringNarrowStart();
+				} else {
+					$(this).parent().css('left', + horizontalMath +'px');
+				}
+			}
+			if( !$(this).hasClass('back-btn')) {
+				$(this).addClass('has-bubble-open')
+				.siblings().removeClass('has-bubble-open');
+			}
+		});
+	}
+
+	function startMentoring() {
+		var wScroll = $(window).scrollTop();
+		// console.log(wScroll);
+		if($('section.mentoring').offset().top - 300 < wScroll) {
+			if( $window.width() > 640 ) {
+				$('.faces').addClass('launched');
+				
+				if( !$('.face').hasClass('has-bubble-open') && $(this).hasClass('back-btn')) {
+					setTimeout(function() {
+						$('.face:nth-child(3)').addClass('has-bubble-open');
+					}, 500);
+				}
+			} else {
+				mentoringNarrowStart();
+			}
+		}
+	}
+
+	function mentoringWideStart() {
+		$('.faces').css({
+			'top': '0',
+			'left': '0'
+		});
+		$('.face:nth-child(3)').addClass('has-bubble-open').siblings().removeClass('has-bubble-open');
+	}
+
+	function mentoringNarrowStart() {
+		$('.faces').css({
+			'top': '230px',
+			'left': '0'
+		});
+		$('.face').first().addClass('has-bubble-open').siblings().removeClass('has-bubble-open');
+	}
+
+})(jQuery);
