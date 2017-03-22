c8675449c6efd05b8fe8996aafc6ed36c13ffbb8
Georgeek <egormad@gmail.com>
2017-03-22 21:31:37 +0800

    photo-container

--- 40-express-generator/bootstrap-express/routes/gallery.js
+++ 40-express-generator/bootstrap-express/routes/gallery.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('gallery', {title: "Georgeek gallery"});
+  res.render('gallery', {pageTitle: "Georgeek gallery"});
 });
 
 module.exports = router;
